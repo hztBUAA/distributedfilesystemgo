45b8a96e14e425343c8206a1078d8c537b300c9b
黄振庭 <21371024@buaa.edu.cn>
2024-06-10 18:13:02 +0800

    strings.Join

--- test/ioReader.go
+++ test/ioReader.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-    "io"
-    "os"
+    "fmt"
     "strings"
 )
 
 func main() {
-    // 创建一个字符串读取器
-    r := strings.NewReader("Hello, World!")
+    // 定义一个字符串切片
+    strs := []string{"Hello", "World", "!"}
 
-    // 打开一个文件用于写入数据
-    f, err := os.Create("test2/output.txt")
-	// f,err := os.MkdirAll("test2/output.txt")
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
+    // 使用 strings.Join 将切片中的所有字符串连接成一个字符串
+    result := strings.Join(strs, ",")
 
-    // 使用 io.Copy 将数据从读取器复制到文件
-    _, err = io.Copy(f, r)
-    if err != nil {
-        panic(err)
-    }
+    // 打印结果
+    fmt.Println(result)
 }
